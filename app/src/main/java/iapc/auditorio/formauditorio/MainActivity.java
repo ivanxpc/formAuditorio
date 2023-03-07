d3ddec170f0cac7f867215d1c596f2956edd210b
IVAN ALEJANDRO PADILLA CORDOVA <nalaivan123@hotmail.com>
2023-03-07 09:31:53 -0600

    Initial commit

---
+++ app/src/main/java/iapc/auditorio/formauditorio/MainActivity.java
@@ -0,0 +1,14 @@
+package iapc.auditorio.formauditorio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
